25ca810773281cec9826e7b5895e5f2616e770bd
m zain ali <mzain42401@gmail.com>
2023-11-27 12:39:46 +0500

    first commit

---
+++ pages/components/Spending.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spending = () => {
+  return (
+    <>
+     spending 
+    </>
+  )
+}
+
+export default Spending
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import Image from 'next/image'
+import React, { useState } from 'react'
+import cld from '../public/calender.png'
+import Spending from './components/Spending'
+import Transaction from './components/Transaction'
 const index = () => {
+
+  const [toggel,setToggle]=useState(false)
   return (
     <>
-      index
+      <section className=' w-full h-screen md:w-[60%] bg-gray-100 m-auto sticky '>
+
+        {/* top============== */}
+
+        <div className='sticky w-full top-0 text-white bg-[#0039a6] h-18 pb-3 flex justify-between items-center '>
+
+          <div className='flex justify-center items-center pt-2'>
+            <div><Image className='w-16 h-12' src={cld} /></div>
+            <h1 className='text-2xl'>My Money Manager</h1>
+          </div>
+
+          <div className='px-10'>
+            dark
+          </div>
+        </div>
+        <div className='w-full sticky bg-[#0039a6] text-white flex justify-evenly items-center h-10 pt-3'>
+          <div onClick={()=>setToggle(false)} className={`hover:border-b-4 cursor-pointer hover:border-[#fff] border-b-4 border-[#0039a6]  ${toggel?null:`border-b-4 border-[#fff]`}`}>
+            SPENDING
+          </div>
+          <div onClick={()=>setToggle(true)}  className={`hover:border-b-4 cursor-pointer hover:border-[#fff] border-b-4 border-[#0039a6] ${toggel?`border-b-4 border-[#fff]`:null}`}>
+            TRANSACTION
+          </div>
+
+        </div>
+
+{
+  toggel?
+  
+<Transaction/>
+  :
+<Spending/>
+}
+
+
+        
+
+      </section>
     </>
   )
 }
