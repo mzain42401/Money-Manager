270d7f18b0ca3bef265eca6be43a67c8c059b6be
m zain ali <mzain42401@gmail.com>
2023-11-27 12:47:00 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const index = () => {
 
           <div className='flex justify-center items-center pt-2'>
             <div><Image className='w-16 h-12' src={cld} /></div>
-            <h1 className='md:text-2xl text-xl '>My Money Manager</h1>
+            <h1 className='md:text-2xl text-xl font-semibold '>My Money Manager</h1>
           </div>
 
           <div className='px-10'>
